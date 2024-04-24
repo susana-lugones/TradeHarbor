07aac8ed0a0896fe6a075f48cc8b9d5608340fa4
Susana <susanalugones@ufl.edu>
2024-04-23 23:04:08 -0400

    everything products related works now

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,35 +1,39 @@
-import React, { useContext } from 'react'
-import './CSS/ShopCategory.css'
-import { ShopContext } from '../Context/ShopContext'
-import dropdown_icon from '../Components/Assets/dropdown_icon.png'
-import Item from '../Components/Item/Item'
+import React, { useContext, useState } from 'react';
+import './CSS/ShopCategory.css';
+import { ShopContext } from '../Context/ShopContext';
+import Item from '../Components/Item/Item';
 
 export const ShopCategory = (props) => {
-  const {all_product} = useContext(ShopContext);
+  const { all_product } = useContext(ShopContext);
+  const [visibleItems, setVisibleItems] = useState(8);
+
+  const loadMoreItems = () => {
+    setVisibleItems(currentVisible => currentVisible + 8); 
+  };
+
+  const displayedProducts = all_product.filter(item => item.category === props.category)
+                                       .slice(0, visibleItems);
+
   return (
     <div className='shop-category'>
       <img className='shopcategory-banner' src={props.banner} alt=""/>
       <div className="shopcategory-indexSort">
         <p>
-          <span>Showing 1-8</span> out of 16 products
+          <span>Showing 1-{Math.min(visibleItems, displayedProducts.length)}</span> out of {all_product.filter(item => item.category === props.category).length} products
         </p>
       </div>
       <div className="shopcategory-products">
-        {all_product.map((item,i)=>{
-          if (props.category===item.category){
-            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-          }
-          else{
-            return null;
-          }
-        })}
-
+        {displayedProducts.map((item, index) => (
+          <Item key={item.id} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+        ))}
       </div>
-      <div className="shopcategory-loadmore">
-        Explore More
-      </div>   
+      {visibleItems < all_product.filter(item => item.category === props.category).length && (
+        <button className="shopcategory-loadmore" onClick={loadMoreItems}>
+          Explore More
+        </button>  
+      )}
     </div>
-  )
+  );
 }
 
-export default ShopCategory
+export default ShopCategory;
